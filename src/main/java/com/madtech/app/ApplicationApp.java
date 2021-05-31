35879a1fa7b91c4abcaaf40c8f1c9467bb217d39
Raju <mtegurhu@gfoundries.com>
2021-05-31 10:37:21 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/madtech/app/ApplicationApp.java
@@ -0,0 +1,18 @@
+package com.madtech.app;
+
+public class ApplicationApp {
+
+	private void add() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void remove() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+	}
+}
